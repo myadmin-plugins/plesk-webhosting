abb6db11f425fdc843208b59cc130421bea92be1
Joe Huss <detain@interserver.net>
2017-07-01 21:24:48 -0400

    adding basic terminate function+hook

--- src/Plugin.php
+++ src/Plugin.php
@@ -24,6 +24,7 @@ class Plugin {
 			self::$module.'.activate' => [__CLASS__, 'getActivate'],
 			self::$module.'.reactivate' => [__CLASS__, 'getReactivate'],
 			self::$module.'.deactivate' => [__CLASS__, 'getDeactivate'],
+			self::$module.'.deactivate' => [__CLASS__, 'getTerminate'],
 		];
 	}
 
@@ -263,6 +264,16 @@ class Plugin {
 	public static function getDeactivate(GenericEvent $event) {
 		if ($event['category'] == SERVICE_TYPES_WEB_PLESK) {
 			myadmin_log(self::$module, 'info', 'Plesk Deactivation', __LINE__, __FILE__);
+			$serviceClass = $event->getSubject();
+			$serviceTypes = run_event('get_service_types', FALSE, self::$module);
+			$settings = get_module_settings(self::$module);
+			$event->stopPropagation();
+		}
+	}
+
+	public static function getTerminate(GenericEvent $event) {
+		if ($event['category'] == SERVICE_TYPES_WEB_PLESK) {
+			myadmin_log(self::$module, 'info', 'Plesk Termination', __LINE__, __FILE__);
 			$serviceClass = $event->getSubject();
 			$serviceTypes = run_event('get_service_types', FALSE, self::$module);
 			$settings = get_module_settings(self::$module);
